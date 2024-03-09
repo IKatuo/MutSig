a1425ad0610cd619cad1b07cf34146652a3efdca
IKatuo <138850983+IKatuo@users.noreply.github.com>
2024-03-09 21:31:32 +0800

    Apply_SigProFiler

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import re
-import pandas as pd
-df=pd.read_excel('/Users/sunyuqi/Desktop/病理科/ruijinbingli/key.xls')
-filtered_df=df['病理诊断']
-key_col=[]
-for i in filtered_df:
-    i=re.findall('“(.*?)”',i)
-    str_all = ','.join(i)
-    key_col.append(str_all)
-key=pd.DataFrame(key_col,columns=['key'])
-print(key)
-key.to_csv('/Users/sunyuqi/Desktop/病理科/ruijinbingli/key.txt', sep='\t',index=False, header = True)
+from SigProfilerExtractor import sigpro as sig
+def main_function():
+ sig.sigProfilerExtractor("vcf", "renal2023_output","/Users/sunyuqi/Desktop/病理科/肾癌2023的副本",reference_genome="GRCh37",opportunity_genome = "GRCh37", context_type = "default", exome = False,minimum_signatures=1,maximum_signatures=4)
+if __name__=="__main__":
+    main_function()
